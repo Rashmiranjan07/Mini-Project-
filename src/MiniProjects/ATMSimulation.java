88db62375650ea907f28470435d7a4e0f3a086fd
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-06-03 23:53:37 +0530

    ATMsimulation

--- src/MiniProjects/ATMSimulation.java
+++ src/MiniProjects/ATMSimulation.java
@@ -7,6 +7,15 @@ public class ATMSimulation {
 		Scanner sc = new Scanner(System.in);
 
         double balance = 1000.0;
+        while (true) {
+        	System.out.println("\n=====ATM MENU=====");
+        	System.out.println("1. Check Balance");
+        	System.out.println("2. Deposit Balance");
+        	System.out.println("3. Withdraw Balance");
+        	System.out.println("4. Exit ");
+        	
+        	System.out.println();
+        }
 	}
 
 }
