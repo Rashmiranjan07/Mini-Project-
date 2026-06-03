d74152a2ec7514970bd504a26b0b56f99184d88c
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-06-03 23:50:24 +0530

    ATMsimulation

--- src/MiniProjects/ATMSimulation.java
+++ src/MiniProjects/ATMSimulation.java
@@ -1,5 +1,12 @@
 package MiniProjects;
 
+import java.util.Scanner;
+
 public class ATMSimulation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        double balance = 1000.0;
+	}
 
 }
