96cdbc5f6655c6169321e1172271620b31560ba8
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-06-04 00:02:02 +0530

    ATMsimulation

--- src/MiniProjects/ATMSimulation.java
+++ src/MiniProjects/ATMSimulation.java
@@ -14,7 +14,40 @@ public class ATMSimulation {
         	System.out.println("3. Withdraw Balance");
         	System.out.println("4. Exit ");
         	
-        	System.out.println();
+        	System.out.println("Enter your choice");
+        	int choice=sc.nextInt();
+        	
+        	switch(choice) {
+        	case 1:
+        		System.out.println("Current Balance: ₹ "+ balance);
+        		break;
+        		
+        	case 2:
+        		System.out.println("Enter amount to deposit");
+        		double deposit=sc.nextDouble();
+        		
+        		if(deposit>0) {
+        			balance+=deposit;
+        			System.out.println("₹"+deposit + "deposited successfully.");
+        			System.out.println("Updated Balance: ₹" + balance);
+        		} else {
+        			System.out.println("Invalid depsoit amount.");
+        		}
+        		break;
+        		
+        	case 3:
+        		System.out.println("Enter amount to withdrae: ₹ ");
+        		double withdraw=sc.nextDouble();
+        		
+        		if(withdraw<=0) {
+        			System.out.println("Invalid withdraw amount :");
+        		} else if ( withdraw > balance) {
+        			System.out.println("Insufficient balance");
+        		} else {
+        			balance-=withdraw;
+        			System.out.println();
+        		}
+        	}
         }
 	}
 
