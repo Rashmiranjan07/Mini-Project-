5e13f2b93a36435600f008be61e606ef8684d3bc
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-06-10 12:37:51 +0530

    Password validator

--- src/MiniProjects/PasswordValidator.java
+++ src/MiniProjects/PasswordValidator.java
@@ -16,35 +16,44 @@ public class PasswordValidator {
 	}
 
 	public static boolean isValidPassword(String password) {
-		if(password.length()<8) {
+		if (password.length() < 8) {
 			System.out.println("❌ Password must be at least 8 characters long.");
 			return false;
 		}
-		boolean hasUpper=false;
-		boolean hasLower= false;
-		boolean hasDigit=false;
-		boolean hasSpecial=false;
-		for( char ch:password.toCharArray()) {
-			if(Character.isUpperCase(ch))
-				hasUpper=true;
-			else if ( Character.isLowerCase(ch)) 
-				hasLower=true;
-			else if(Character.isDigit(ch))
-				hasDigit=true;
-			else 
-				hasSpecial=true;
-				
-			
+		boolean hasUpper = false;
+		boolean hasLower = false;
+		boolean hasDigit = false;
+		boolean hasSpecial = false;
+		for (char ch : password.toCharArray()) {
+			if (Character.isUpperCase(ch))
+				hasUpper = true;
+			else if (Character.isLowerCase(ch))
+				hasLower = true;
+			else if (Character.isDigit(ch))
+				hasDigit = true;
+			else
+				hasSpecial = true;
+
 		}
-		
-		if(!hasUpper) {
+
+		if (!hasUpper) {
 			System.out.println("❌ Password must contain at least one uppercase letter.");
-		} 
-		if (!hasLower) {
-			
+			return false;
 		}
-		return false;
+		if (!hasLower) {
+			System.out.println("❌ Password must contain at least one lowercase letter.");
+			return false;
+		}
+
+		if (!hasDigit) {
+			System.out.println("❌ Password must contain at least one Digit.");
+			return false;
+		}
+		if (!hasSpecial) {
+			System.out.println("❌ Password must contain at least one special character.");
+			return false;
+		}
+		return true;
 	}
 
 }
-
