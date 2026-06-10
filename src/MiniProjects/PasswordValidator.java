3f331c99cccb1ba0d279e2b80318a9bff8bfbdcb
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-06-10 12:38:59 +0530

    Password validator

--- src/MiniProjects/PasswordValidator.java
+++ src/MiniProjects/PasswordValidator.java
@@ -3,6 +3,7 @@ package MiniProjects;
 import java.util.Scanner;
 
 public class PasswordValidator {
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a password");
@@ -20,10 +21,12 @@ public class PasswordValidator {
 			System.out.println("❌ Password must be at least 8 characters long.");
 			return false;
 		}
+		
 		boolean hasUpper = false;
 		boolean hasLower = false;
 		boolean hasDigit = false;
 		boolean hasSpecial = false;
+		
 		for (char ch : password.toCharArray()) {
 			if (Character.isUpperCase(ch))
 				hasUpper = true;
